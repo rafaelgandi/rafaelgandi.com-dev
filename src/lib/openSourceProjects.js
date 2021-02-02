73dc2b560b7e7327522b76f0cf907b921e80321f
Raffy <raffy@vectto.com>
2021-02-03 06:45:07 +0800

    remove old open source projs

--- src/lib/openSourceProjects.js
+++ src/lib/openSourceProjects.js
@@ -6,18 +6,6 @@ export default [
         "image" : "/images/proj/minali.png"
     },
     {
-        "header" : "Wasabi Artisan",
-        "desc" : "Wasabi Artisan", 
-        "link" : "https://github.com/rafaelgandi/wasabi_artisan",
-        "image" : "/images/proj/wasabilogo.png"
-    },
-    {
-        "header" : "Boot.js",
-        "desc" : "Boot.js - A Javascript cache and loader.",
-        "link" : "https://github.com/rafaelgandi/Bootjs",
-        "image" : "/images/proj/bootjs.jpg"
-    },
-    {
         "header" : "RunWhen",
         "desc" : "RunWhen - Javascript code dependency checker.", 
         "link" : "http://rafaelgandi.github.io/RunWhen",
@@ -28,11 +16,5 @@ export default [
         "desc" : "Monthlyst - My simple personal monthly todo list android app.", 
         "link" : "http://rafaelgandi.github.io/monthlyst",
         "image" : "/images/proj/monthlyst.png"
-    },
-    {
-        "header" : "StashLoader.js",
-        "desc" : "StashLoader.js - Cache scripts in localStorage for less http requests.", 
-        "link" : "https://github.com/rafaelgandi/StashLoader",
-        "image" : "/images/proj/stashloader.png"
     }
 ];
